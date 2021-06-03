4a42ff0c412e46f2a995c044c30d05d7a297ae8c
BikrantaS <bikrantasarkar@gmail.com>
2021-06-03 14:51:10 +0530

    Heroku webapi

---
+++ app.js
@@ -0,0 +1,63 @@
+// const http = require('http');
+
+// const hostname = '0.0.0.0';
+// const port = 3000;
+
+// let c = 0;
+// const server = http.createServer((req, res) => {
+//     console.log('request no.:', c++);
+
+//     if (req.url === '/') {
+//         res.end("Welcome to homepage");
+//     }
+//     else if (req.url === '/about') {
+//         res.end("About page");
+//     }
+//     else {
+//         res.end("<H1>OPPS</H1>");
+//     }
+
+// })
+
+// server.listen(port, hostname, () => {
+//     console.log(`server on http://${hostname}:${port}..`);
+// })
+
+const hostname = '0.0.0.0';
+const port =process.env.PORT || 3000;
+
+
+
+const express = require('express');
+const logger = require('./logger');//importing defined middleware
+const authorize = require('./authorize');
+const data = require('./data');
+
+const app = express();
+app.use([logger, authorize]);
+// app.use(express.static('./YOUR STATIC DIRECTORY'));  //to access staticfiles
+
+app.get('/', logger, (req, res) => {
+    res.send("WELCOME TO ROOT");
+})
+
+app.get('/about', (req, res) => {
+    res.send("API");
+})
+
+app.get('/api/v1/:info', (req, res) => {
+    const { info } = req.params;
+    console.log("id:", info);
+    res.status(200).json({ sucess: true, data: data });
+    //res.send("ABout PAGE");
+})
+
+app.all('*', (req, res) => {
+    res.send("INAVLID URL");
+})
+
+
+
+app.listen(port, () => {
+    console.log("Server running on port ${port}..")
+})
